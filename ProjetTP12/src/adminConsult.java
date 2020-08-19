b11287c6a28abc32730bb72e08bbecee47b2cc37
khitermedachraf <khiter.med7@gmail.com>
2020-08-19 01:56:54 +0100

    Building the adminConsult Frame for shwing Data in table{ done}

--- ProjetTP12/src/adminConsult.java
+++ ProjetTP12/src/adminConsult.java
@@ -3,6 +3,8 @@ import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.Image;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -10,6 +12,14 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import net.proteanit.sql.DbUtils;
+
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTable;
+import javax.swing.JScrollPane;
 
 public class adminConsult extends JFrame {
 
@@ -31,13 +41,14 @@ public class adminConsult extends JFrame {
 		});
 	}
 	Connection connection = null ; 
+	private JTable table;
 	/**
 	 * Create the frame.
 	 */
 	public adminConsult() {
 		connection = sqliteConnection.dbConnector(); 
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 929, 654);
+		setBounds(100, 100, 1176, 668);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -60,7 +71,7 @@ public class adminConsult extends JFrame {
 		Image imgLogout = new ImageIcon(this.getClass().getResource("/logout.png")).getImage ();
 		btnNewButton.setIcon(new ImageIcon(imgLogout));
 		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 20));
-		btnNewButton.setBounds(730, 37, 163, 50);
+		btnNewButton.setBounds(976, 30, 163, 50);
 		contentPane.add(btnNewButton);
 		
 		JLabel lblNewLabel = new JLabel("1- Select Data from Student table :");
@@ -69,7 +80,8 @@ public class adminConsult extends JFrame {
 		lblNewLabel.setBounds(48, 143, 361, 50);
 		contentPane.add(lblNewLabel);
 		
-		JLabel lblNewLabel_1 = new JLabel("New label");
+		JLabel lblNewLabel_1 = new JLabel("SELECT *  FROM  Etudiant ;");
+		lblNewLabel_1.setForeground(Color.BLUE);
 		lblNewLabel_1.setBounds(93, 196, 319, 20);
 		contentPane.add(lblNewLabel_1);
 		
@@ -79,9 +91,10 @@ public class adminConsult extends JFrame {
 		lblSelectData.setBounds(48, 244, 361, 50);
 		contentPane.add(lblSelectData);
 		
-		JLabel label = new JLabel("New label");
-		label.setBounds(90, 298, 319, 20);
-		contentPane.add(label);
+		JLabel lblSelectFrom = new JLabel("SELECT * FROM Enseignant ;");
+		lblSelectFrom.setForeground(Color.BLUE);
+		lblSelectFrom.setBounds(90, 298, 319, 20);
+		contentPane.add(lblSelectFrom);
 		
 		JLabel lblSelectData_1 = new JLabel("3- Select Data from Unite table :");
 		lblSelectData_1.setToolTipText("1- Select Data from Student table :");
@@ -89,19 +102,136 @@ public class adminConsult extends JFrame {
 		lblSelectData_1.setBounds(48, 363, 361, 50);
 		contentPane.add(lblSelectData_1);
 		
-		JLabel label_1 = new JLabel("New label");
-		label_1.setBounds(90, 424, 319, 20);
-		contentPane.add(label_1);
+		JLabel lblSelectFrom_1 = new JLabel("SELECT * FROM Unite ;");
+		lblSelectFrom_1.setForeground(Color.BLUE);
+		lblSelectFrom_1.setBounds(90, 424, 319, 20);
+		contentPane.add(lblSelectFrom_1);
 		
-		JLabel lblSelectData_2 = new JLabel("4- Select Data from Unite table :");
+		JLabel lblSelectData_2 = new JLabel("4- Select Data from Student-Unite table :");
 		lblSelectData_2.setToolTipText("1- Select Data from Student table :");
 		lblSelectData_2.setFont(new Font("Tahoma", Font.ITALIC, 20));
-		lblSelectData_2.setBounds(48, 487, 361, 50);
+		lblSelectData_2.setBounds(48, 487, 400, 50);
 		contentPane.add(lblSelectData_2);
 		
-		JLabel label_3 = new JLabel("New label");
-		label_3.setBounds(93, 530, 319, 20);
-		contentPane.add(label_3);
+		JLabel lblSelectFrom_2 = new JLabel("SELECT * FROM EtudiantUnite ;");
+		lblSelectFrom_2.setForeground(Color.BLUE);
+		lblSelectFrom_2.setBounds(93, 530, 319, 20);
+		contentPane.add(lblSelectFrom_2);
+		
+		JButton btnStudent = new JButton("Show Data");
+		Image imgShow = new ImageIcon(this.getClass().getResource("/showbtn.png")).getImage ();
+		btnStudent.setIcon(new ImageIcon(imgShow));
+		btnStudent.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// show Data for  student table 
+				try {
+					String query = " SELECT *  FROM  Etudiant ;";
+					PreparedStatement prprStat = connection.prepareStatement(query) ;
+					ResultSet result = prprStat.executeQuery();
+					table.setModel(DbUtils.resultSetToTableModel(result));
+					// close connection with data base .
+					result.close();
+					prprStat.close();
+				}catch (Exception e1) {
+					e1.printStackTrace();
+				}
+				
+			}
+		});
+		btnStudent.setFont(new Font("Tahoma", Font.BOLD, 20));
+		btnStudent.setBounds(455, 166, 209, 50);
+		contentPane.add(btnStudent);
+		
+		JButton buttonTeacher = new JButton("Show Data");
+		buttonTeacher.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// show Data for  Teacher table 
+				try {
+					String query = " SELECT *  FROM  Enseignant ;";
+					PreparedStatement prprStat = connection.prepareStatement(query) ;
+					ResultSet result = prprStat.executeQuery();
+					table.setModel(DbUtils.resultSetToTableModel(result));
+					// close connection with data base .
+					result.close();
+					prprStat.close();
+				}catch (Exception e1) {
+					e1.printStackTrace();
+				}
+			}
+		});
+		buttonTeacher.setIcon(new ImageIcon(imgShow));
+		buttonTeacher.setFont(new Font("Tahoma", Font.BOLD, 20));
+		buttonTeacher.setBounds(455, 268, 209, 50);
+		contentPane.add(buttonTeacher);
+		
+		JButton button = new JButton("Show Data");
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// show Data for  Unite table 
+				try {
+					String query = " SELECT *  FROM  Unite ;";
+					PreparedStatement prprStat = connection.prepareStatement(query) ;
+					ResultSet result = prprStat.executeQuery();
+					table.setModel(DbUtils.resultSetToTableModel(result));
+					// close connection with data base .
+					result.close();
+					prprStat.close();
+				}catch (Exception e1) {
+					e1.printStackTrace();
+				}
+				
+			}
+		});
+		button.setIcon(new ImageIcon(imgShow));
+		button.setFont(new Font("Tahoma", Font.BOLD, 20));
+		button.setBounds(325, 421, 209, 50);
+		contentPane.add(button);
+		
+		JButton button_1 = new JButton("Show Data");
+		button_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// show Data for  Etudiant-Unite table 
+				try {
+					String query = " SELECT *  FROM  EtudiantUnite ;";
+					PreparedStatement prprStat = connection.prepareStatement(query) ;
+					ResultSet result = prprStat.executeQuery();
+					table.setModel(DbUtils.resultSetToTableModel(result));
+					// close connection with data base .
+					result.close();
+					prprStat.close();
+				}catch (Exception e1) {
+					e1.printStackTrace();
+				}
+			}
+		});
+		button_1.setIcon(new ImageIcon(imgShow));
+		button_1.setFont(new Font("Tahoma", Font.BOLD, 20));
+		button_1.setBounds(325, 532, 209, 50);
+		contentPane.add(button_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("New label");
+		Image imgLblShow = new ImageIcon(this.getClass().getResource("/show.png")).getImage ();
+		lblNewLabel_2.setIcon(new ImageIcon(imgLblShow));
+		lblNewLabel_2.setBounds(961, 172, 131, 122);
+		contentPane.add(lblNewLabel_2);
+		
+		JLabel label = new JLabel("New label");
+		Image imgLbl = new ImageIcon(this.getClass().getResource("/search.png")).getImage ();
+		label.setIcon(new ImageIcon(imgLbl));
+		label.setBounds(771, 172, 131, 122);
+		contentPane.add(label);
+		
+		JLabel lblNewLabel_3 = new JLabel("#Bulletin board# :");
+		lblNewLabel_3.setFont(new Font("Tahoma", Font.BOLD, 20));
+		lblNewLabel_3.setBounds(567, 347, 192, 43);
+		contentPane.add(lblNewLabel_3);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(567, 408, 572, 190);
+		contentPane.add(scrollPane);
+		
+		table = new JTable();
+		scrollPane.setViewportView(table);
 		
 		
 	}
